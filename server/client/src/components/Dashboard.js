ef27d27757bfeee33da7e91cbd450566a18cfc1a
Emre Çolak <emrecolak@Emre.home>
2023-02-18 21:05:09 +0300

    add preloader component

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -6,21 +6,30 @@ import { useThunk } from '../hooks/use-thunk';
 import { fetchSurveys } from '../store';
 
 import SurveyList from './surveys/SurveyList';
+import Preloader from './Preloader';
 
 function Dashboard() {
   const surveys = useSelector((state) => {
     return state.survey.data;
   });
 
-  const [doFetchSurveys, ,] = useThunk(fetchSurveys);
+  const [doFetchSurveys, isLoading] = useThunk(fetchSurveys);
 
   useState(() => {
     doFetchSurveys();
   }, [doFetchSurveys]);
 
+  let content;
+
+  if (isLoading) {
+    content = <Preloader />;
+  } else {
+    content = <SurveyList surveys={surveys} />;
+  }
+
   return (
     <div>
-      <SurveyList surveys={surveys} />
+      {content}
       <div className="fixed-action-btn">
         <Link to="/surveys/new" className="btn-floating btn-large red">
           <i className="material-icons">add</i>
---
+++ server/client/src/components/Preloader.js
@@ -0,0 +1,11 @@
+function Preloader() {
+  return (
+    <div className="center-align" style={{ height: '100%' }}>
+      <div className="progress">
+        <div className="indeterminate"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Preloader;
